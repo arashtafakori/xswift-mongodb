7cbeb05c9c15766c6a6526d556fbee78bec6d2e8
arashtafakori <arash.software.dev@gmail.com>
2023-10-31 21:39:16 +0300

    the setting classes were moved to this project and a test project was added too.

---
+++ src/MongoDBSettings.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MongoDB.XSwift
+{
+    public class MongoDBSettings
+    {
+ 
+        private IConfigurationRoot? _configuration;
+
+        public string? _connectString;
+        public string ConnectString
+        {
+            get => _connectString!;
+        }
+
+        public MongoDBSettings()
+        {
+
+        }
+        public MongoDBSettings(IConfigurationRoot configuration)
+        {
+            _configuration = configuration;
+
+            _connectString = _configuration
+                .GetSection("MongoDBSettings")
+                .GetSection("ConnectionStrings").Value!;
+        }
+
+        public void SetMongoBdConnectString(string value)
+        {
+            _connectString = value;
+        }
+    }
+}
